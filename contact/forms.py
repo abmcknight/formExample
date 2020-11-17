332ebb255b85192565d78da8f9266ce152bc7bae
CS416F19 <cs416f19@gmail.com>
2020-11-16 22:28:05 -0500

    Updated with the form elements with Bootstrap class

--- contact/forms.py
+++ contact/forms.py
@@ -7,3 +7,10 @@ class ContactForm(forms.ModelForm):
         model = Contact
         # fields = ['first_name', 'last_name', 'email', 'message'] # individual fields can be included this way
         fields = '__all__'  # includes all the fields from the model
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'})
+    }
